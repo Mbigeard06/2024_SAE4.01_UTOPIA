fb4b9cda96f73686bc316bd5e86d012bb079ddf1
ClementBoutet <clement.boutet@iut-dijon.u-bourgogne.fr>
2024-05-31 17:15:48 +0200

    affichage du css et du contenu

--- views/gabarit.php
+++ views/gabarit.php
@@ -1,20 +1,18 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="A Social Media Website" />
+    <meta name="keywords" content="klik, social-media, social-network, forum">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/footer.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
+    <link rel="shortcut icon" href="img/favicon.png" />
 </head>
+
 <body>
-    <header>
-
-        
-    </header>
-    <main id="contenu">
-        <?= $contenu ?>
-    </main>
-
-    <footer>
-
-    </footer>
+    <?= $contenu ?>
 </body>
+
 </html>
--- views/view.php
+++ views/view.php
@@ -48,4 +48,4 @@ class view{
 }
 
 
-?>s
+?>
